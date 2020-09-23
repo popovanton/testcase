52670335400b3fbffc771006f421ef61cca5e555
popovanton <mr.anpopov@mail.ru>
2020-09-23 08:07:30 +0300

    dynamic bean processing added for possible prototype beans

--- src/main/java/com/anpopov/testcase/service/NotificationServiceImpl.java
+++ src/main/java/com/anpopov/testcase/service/NotificationServiceImpl.java
@@ -2,6 +2,8 @@ package com.anpopov.testcase.service;
 
 import com.anpopov.testcase.annotation.Subscriber;
 import com.anpopov.testcase.subscriber.IEventSubscriber;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
@@ -14,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class NotificationServiceImpl implements NotificationService, ApplicationListener<ApplicationReadyEvent> {
+public class NotificationServiceImpl implements NotificationService, ApplicationListener<ApplicationReadyEvent>, BeanPostProcessor {
 
     private final ApplicationContext applicationContext;
     private final Map<String, List<IEventSubscriber>> annotatedBeans = new HashMap<>();
@@ -32,9 +34,32 @@ public class NotificationServiceImpl implements NotificationService, Application
     }
 
     @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof IEventSubscriber) {
+            IEventSubscriber castedBean = (IEventSubscriber) bean;
+            Subscriber annotation = bean.getClass().getAnnotation(Subscriber.class);
+            if (annotation != null) {
+                for (String t : annotation.themes()) {
+                    List<IEventSubscriber> iEventSubscribers = annotatedBeans.get(t);
+                    if (iEventSubscribers != null) {
+                        iEventSubscribers.add(castedBean);
+                    } else {
+                        LinkedList<IEventSubscriber> eventSubscribersList = new LinkedList<>();
+                        eventSubscribersList.add(castedBean);
+                        annotatedBeans.put(t, eventSubscribersList);
+                    }
+                }
+            }
+        }
+        return bean;
+    }
+
+        @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(Subscriber.class);
+
         beansWithAnnotation.values().forEach(it -> {
+
             String[] themes = it.getClass().getAnnotation(Subscriber.class).themes();
             for (String t : themes) {
                 List<IEventSubscriber> objects = annotatedBeans.get(t);
--- src/test/java/com/anpopov/testcase/TestcaseApplicationTests.java
+++ src/test/java/com/anpopov/testcase/TestcaseApplicationTests.java
@@ -4,9 +4,13 @@ import com.anpopov.testcase.annotation.Subscriber;
 import com.anpopov.testcase.service.NotificationService;
 import com.anpopov.testcase.subscriber.IEventSubscriber;
 import com.anpopov.testcase.testModels.MessageWithProperties;
+import com.anpopov.testcase.testModels.subscriber.SportSubscriber;
 import com.anpopov.testcase.testModels.subscriber.SubscriberObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
@@ -21,6 +25,9 @@ class TestcaseApplicationTests {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private DefaultListableBeanFactory beanFactory;
 
     @Test
     public void testOkMessageReceivedAndObjectIsRetrievedThemeIsCars() {
@@ -57,6 +64,40 @@ class TestcaseApplicationTests {
         Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(Subscriber.class);
         Object[] objects = beansWithAnnotation.values().toArray();
         for (Object object : objects) {
+            IEventSubscriber subscriber = (IEventSubscriber) object;
+            String[] themes = subscriber.getClass().getAnnotation(Subscriber.class).themes();
+            SubscriberObject subscriberObject = (SubscriberObject) object;
+            if (themes[0].equals("sport")) {
+                assertTrue(subscriberObject.isMessageReceived());
+                assertEquals(subscriberObject.getObject(), messageWithProperties);
+            } else {
+                assertFalse(subscriberObject.isMessageReceived());
+                assertNull(subscriberObject.getObject());
+            }
+        }
+    }
+
+    @Test
+    public void testOkPrototypeBeanDynamicallyCreatedMessageReceivedAndObjectIsRetrievedThemeIsSport() {
+        NotificationService notificationService =
+                (NotificationService) applicationContext.getBean("notificationServiceImpl");
+
+        MessageWithProperties messageWithProperties = new MessageWithProperties("anton", "popov");
+
+        Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(Subscriber.class);
+        Object[] beanList = beansWithAnnotation.values().toArray();
+
+        assertEquals(3, beanList.length);
+
+        beanFactory.registerBeanDefinition("sportSubscriberero", BeanDefinitionBuilder.genericBeanDefinition(SportSubscriber.class).setScope("prototype").getBeanDefinition());
+
+        Map<String, Object> beansWithAnnotation1 = applicationContext.getBeansWithAnnotation(Subscriber.class);
+        Object[] updatedBeanList = beansWithAnnotation1.values().toArray();
+        assertEquals(4, updatedBeanList.length);
+
+        notificationService.notify("sport", messageWithProperties);
+
+        for (Object object : updatedBeanList) {
             IEventSubscriber subscriber = (IEventSubscriber) object;
             String[] themes = subscriber.getClass().getAnnotation(Subscriber.class).themes();
             SubscriberObject subscriberObject = (SubscriberObject) object;
